1d19ef197d80ea28da02ccad0eabae2408fbf0bd
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-09-12 14:29:37 +0300

    create new directory

---
+++ chapter1/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number):string => {
+    let BMI = weight / ((height/100) ** 2)
+    if (BMI < 18.5) {
+        return 'underweight'
+    } else if (BMI < 24.9) {
+        return 'normal weight'
+    } else if (BMI < 29.9) {
+        return 'overweight'
+    } else {
+        return 'obese'
+    } 
+}
+
+console.log(calculateBmi(180,74))
+
+export default calculateBmi
