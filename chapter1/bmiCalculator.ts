e4fc89b4091ebffe57f820d2fbe11915864adaf6
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-09-15 12:19:30 +0300

    9.1 and 9.2 now work with inputs from the command line

--- chapter1/bmiCalculator.ts
+++ chapter1/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface inputValues {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: string[]): inputValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!')
+    }
+}
+
 const calculateBmi = (height: number, weight: number):string => {
     let BMI = weight / ((height/100) ** 2)
     if (BMI < 18.5) {
@@ -11,6 +30,20 @@ const calculateBmi = (height: number, weight: number):string => {
     } 
 }
 
-console.log(calculateBmi(180,74))
+
+
+try {
+    if (require.main === module) {
+    const {height, weight} = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight))
+}
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong. '
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message
+    }
+    console.log(errorMessage)
+}
+
 
 export default calculateBmi
--- chapter1/exerciseCalculator.ts
+++ chapter1/exerciseCalculator.ts
@@ -9,12 +9,32 @@ interface Result {
 
 
 }
+
+interface ParsedArguments {
+    target: number
+    dailyExercise: number[]
+}
+
+const parseArguments = (args: string[]): ParsedArguments => {
+    const numberList = []
+    if (args.length < 4) throw new Error('not enough arguments')
+    if (isNaN(Number(args[2]))) throw new Error('target is invalid')
+    for (let i = 3; i < args.length; i++) {
+        if (isNaN(Number(args[i]))) throw new Error('Invalid input')
+            numberList.push(Number(args[i]))
+    }
+    return {
+        target: Number(args[2]),
+        dailyExercise: numberList
+    }
+}
+
 const calculateExercises = (dailyExercise: number[], target: number): Result => {
     const average = dailyExercise.reduce((sum, value) => sum + value,0) /dailyExercise.length
     return {
         periodLength: dailyExercise.length,
         trainingDays: dailyExercise.filter((hoursWorked) => hoursWorked > 0).length,
-        success: average > target,
+        success: average >= target,
         rating: 0,
         ratingDescription: 'You can always work more!!!',
         target: target,
@@ -22,4 +42,14 @@ const calculateExercises = (dailyExercise: number[], target: number): Result => 
     }
 }
 
-    console.log(calculateExercises([2,4,5,0,1,0,0,0], 4))
+    
+    try {
+        const {target, dailyExercise} = parseArguments(process.argv)
+        console.log(calculateExercises(dailyExercise, target))
+    } catch (error: unknown) {
+        let errorMessage = 'Something went wrong. '
+        if (error instanceof Error) {
+            errorMessage += 'Error: ' + error.message
+        }
+        console.log(errorMessage)
+    }
